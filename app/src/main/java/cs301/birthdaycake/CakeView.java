278e0b726d93c47387a411c63e7199dc1b7d1d32
moreym26 <moreym25@up.edu>
2023-01-26 18:29:42 -0800

    Happy Birthday

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -120,7 +120,8 @@ public class CakeView extends SurfaceView {
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
         //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
+        drawCandle(canvas, cakeLeft/2 + cakeWidth/2 - candleWidth/2, cakeTop);
+        drawCandle(canvas, cakeLeft + cakeLeft/2 + cakeWidth/2 - candleWidth/2, cakeTop);
 
     }//onDraw
 
